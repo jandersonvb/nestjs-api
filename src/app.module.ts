fe3a32808dee85a9b8e01c4d911b286de8daf329
jandersonvb <jandersonvb.dev@gmail.com>
2024-02-07 21:15:29 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest_api',
+      entities: [Product],
+      synchronize: true,
+      logging: true,
+    }),
+
+    ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
